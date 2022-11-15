ae7f87fcad39f769df47b07dbd2035cb3c2410ee
Andrei Hutsau <mwplt@mail.ru>
2022-11-15 13:04:36 +0300

    map method

--- src/Components/Body.tsx
+++ src/Components/Body.tsx
@@ -1,10 +1,21 @@
+import {NewComponents} from "./NewComponents";
 
 type PropsType={
     title:string
 }
 
 export const Body=(props:PropsType)=>{
+    const students=[
+        {id:1,name:"Igor",age:33},
+        {id:2,name:"Ivan",age:13},
+        {id:3,name:"Andru",age:45},
+    ]
+
     return(
-       <div>{props.title}</div>
+        <>
+            <div>{props.title}</div>
+            <NewComponents students={students}/>
+        </>
+
     )
 }
---
+++ src/Components/NewComponents.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../App.css"
+
+type NewComponentsType = {
+    students: StudentType[]
+}
+type StudentType = {
+    id: number
+    name: string
+    age: number
+}
+
+export const NewComponents = (props: NewComponentsType) => {
+    const topCars = [
+        {manufacturer:'BMW', model:'m5cs'},
+        {manufacturer:'Mercedes', model:'e63s'},
+        {manufacturer:'Audi', model:'rs6'}
+    ]
+    return (
+        <>
+            <div className={"forMap"}>new components</div>
+            <ul>
+                {props.students.map((el) => {
+                    return (
+                        <li key={el.id}>
+                            {`${el.name} - ${el.age}`}
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <table width="100%" cellSpacing="0" border={1}  >
+                {
+                    topCars.map((el)=>{
+                        return(
+                            <tr >
+                                <th>{el.manufacturer}</th>
+                                <th>{el.model}</th>
+                            </tr>
+                        )
+                    })
+                }
+            </table>
+
+        </>
+
+    )
+}
